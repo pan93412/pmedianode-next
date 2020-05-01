27e9b2cd0d572aa58db6b5edd1fe42c7483bfee4
pan93412 <pan93412@gmail.com>
2020-05-02 01:44:18 +0800

    feat(pages/_app): add the viewport

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,15 @@
 import './bulma.scss';
 import { AppProps } from 'next/app';
+import Head from 'next/head';
 
 export default function MainApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <div>
+      <Head>
+        <meta name='viewport' content='width=device-width, initial-scale=1.0' />
+        
+      </Head>
+      <Component {...pageProps} />
+    </div>
+  )
 }
