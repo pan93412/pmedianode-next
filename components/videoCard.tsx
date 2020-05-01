a970be0a73385a9736705a838275f9f2d3ebad2b
pan93412 <pan93412@gmail.com>
2020-05-01 19:34:23 +0800

    feat(comp/videoCard): remove uploadedOn

--- components/videoCard.tsx
+++ components/videoCard.tsx
@@ -1,26 +1,23 @@
 import {
     Card, CardContent, Content, Title
 } from 'bloomer';
-import formatDate from '../components/formatDate';
 import { CardFooter } from 'bloomer/lib/components/Card/Footer/CardFooter';
 import { CardFooterItem } from 'bloomer/lib/components/Card/Footer/CardFooterItem';
 import Link from 'next/link';
 
-export interface NavProps {
+export interface INavProps {
     id: string;
     title: string;
     desc: string;
-    uploadedOn: Date;
 }
 
-export default function nav(props: NavProps) {
+export default function nav(props: INavProps) {
     return (
         <Card style={{ maxWidth: '15em' }}>
             <CardContent>
                 <Content>
                     <Title isSize={3}>{props.title}</Title>
                     <p>{props.desc}</p>
-                    <small>{formatDate(props.uploadedOn)}</small>
                 </Content>
             </CardContent>
             <CardFooter>
