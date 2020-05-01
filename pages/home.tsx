68e9e06298b0ed83368e0a1395750b50d0694739
pan93412 <pan93412@gmail.com>
2020-05-01 20:19:09 +0800

    feat(pages/home): getting video data with API

--- pages/home.tsx
+++ pages/home.tsx
@@ -1,25 +1,36 @@
 import Head from 'next/head';
-import { Section, Container, Button, Title } from 'bloomer';
+import { Section, Container, Title } from 'bloomer';
 import Nav from '../components/nav';
+import getVideo, { IVideoMeta } from '../libs/getVideo';
 import VideoCard from '../components/videoCard';
 import { Columns } from 'bloomer/lib/grid/Columns';
 import { Column } from 'bloomer/lib/grid/Column';
 import { Subtitle } from 'bloomer/lib/elements/Subtitle';
+import { useState } from 'react';
 
 export default function Home() {
+  const [videoData, setVideoData] = useState([] as IVideoMeta[]);
+  getVideo().then(d => d.videos).then(d => {
+    setVideoData(d);
+  });
+
   return (
     <div className="container">
       <Head>
         <title>pMediaNode / 首頁</title>
       </Head>
-      <Nav pageTitle='首頁' user='admin'/>
+      <Nav pageTitle='首頁'/>
       <Section>
         <Container>
           <Columns>
             <Column>
               <Title>最新影片</Title>
               <Subtitle>時刻保持最新。</Subtitle>
-              <VideoCard id='test' title='測試' desc='測試' uploadedOn={new Date()}></VideoCard>
+              {
+                videoData.map(meta => (
+                  <VideoCard id={meta.id} title={meta.title} desc={meta.desc}></VideoCard>
+                ))
+              }
             </Column>
             <Column>
               <Title>熱門影片</Title>
