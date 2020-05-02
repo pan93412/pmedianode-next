03c406a491342584e267f8577cb69065cace5b65
pan93412 <pan93412@gmail.com>
2020-05-02 19:23:59 +0800

    feat(pages/api): use the generic api method

--- pages/api/get-video.ts
+++
@@ -1,9 +0,0 @@
-import { NextApiRequest, NextApiResponse } from 'next';
-import getVideos from '../../libs/getVideo';
-
-export default function getVideoAPI(req: NextApiRequest, res: NextApiResponse) {
-  res.status(200).json({
-    status: true,
-    videos: getVideos().videos,
-  });
-}
---
+++ pages/api/get/[type].ts
@@ -0,0 +1,39 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+import getVideo from '../../../libs/getVideo';
+import getAudio from '../../../libs/getAudio';
+
+export function getVideoAPI(req: NextApiRequest, res: NextApiResponse) {
+  res.status(200).json({
+    status: true,
+    ...getVideo(),
+  });
+}
+
+export function getAudioAPI(req: NextApiRequest, res: NextApiResponse) {
+  res.status(200).json({
+    status: true,
+    ...getAudio(),
+  });
+}
+
+export default function getAPI(req: NextApiRequest, res: NextApiResponse) {
+  const {
+    query: { type },
+  } = req;
+
+  if (type === 'video') getVideoAPI(req, res);
+  else if (type === 'audio') getAudioAPI(req, res);
+  else {
+    res.status(400).json({
+      status: false,
+      errors: {
+        code: 'NO_SUCH_METHOD',
+        msg: `No such method: ${type}`,
+      },
+    });
+  }
+}
+
+export async function getStaticPaths() {
+  return ['video', 'audio'];
+}
