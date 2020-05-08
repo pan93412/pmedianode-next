ce386d2745c2b8052e7ddefe24e5f692f7d7c43a
pan93412 <pan93412@gmail.com>
2020-05-08 14:20:02 +0800

    feat(comps/nav): automatically set title

--- components/nav.tsx
+++ components/nav.tsx
@@ -11,6 +11,7 @@ import {
 } from 'bloomer';
 import React, { useState } from 'react';
 import Link from 'next/link';
+import Head from 'next/head';
 
 export interface NavProps {
   pageTitle: string;
@@ -24,32 +25,41 @@ export default function nav({
   const setIsActive = () => _setIsActive(!isActive);
 
   return (
-    <Navbar>
-      <NavbarBrand>
-        <NavbarItem>
+    <div>
+      <Head>
+        <title>
           pMediaNode |
           {' '}
           {pageTitle}
-        </NavbarItem>
-        <NavbarBurger isActive={isActive} onClick={setIsActive} />
-      </NavbarBrand>
-      <NavbarMenu isActive={isActive} onClick={setIsActive}>
-        <NavbarStart>
-          <Link href="/home"><NavbarItem href="#">首頁</NavbarItem></Link>
-          <Link href="/library/video"><NavbarItem href="#">影片</NavbarItem></Link>
-          <Link href="/library/audio"><NavbarItem href="#">音樂</NavbarItem></Link>
-          <Link href="/library/announce"><NavbarItem href="#">公告</NavbarItem></Link>
-        </NavbarStart>
-        <NavbarEnd>
-          <NavbarItem hasDropdown isHoverable>
-            <NavbarLink>設定 & 關於</NavbarLink>
-            <NavbarDropdown>
-              <Link href="/preferences"><NavbarItem href="#">偏好設定</NavbarItem></Link>
-              <Link href="/about"><NavbarItem href="#">關於</NavbarItem></Link>
-            </NavbarDropdown>
+        </title>
+      </Head>
+      <Navbar>
+        <NavbarBrand>
+          <NavbarItem>
+            pMediaNode |
+            {' '}
+            {pageTitle}
           </NavbarItem>
-        </NavbarEnd>
-      </NavbarMenu>
-    </Navbar>
+          <NavbarBurger isActive={isActive} onClick={setIsActive} />
+        </NavbarBrand>
+        <NavbarMenu isActive={isActive} onClick={setIsActive}>
+          <NavbarStart>
+            <Link href="/home"><NavbarItem href="#">首頁</NavbarItem></Link>
+            <Link href="/library/video"><NavbarItem href="#">影片</NavbarItem></Link>
+            <Link href="/library/audio"><NavbarItem href="#">音樂</NavbarItem></Link>
+            <Link href="/library/announce"><NavbarItem href="#">公告</NavbarItem></Link>
+          </NavbarStart>
+          <NavbarEnd>
+            <NavbarItem hasDropdown isHoverable>
+              <NavbarLink>設定 & 關於</NavbarLink>
+              <NavbarDropdown>
+                <Link href="/preferences"><NavbarItem href="#">偏好設定</NavbarItem></Link>
+                <Link href="/about"><NavbarItem href="#">關於</NavbarItem></Link>
+              </NavbarDropdown>
+            </NavbarItem>
+          </NavbarEnd>
+        </NavbarMenu>
+      </Navbar>
+    </div>
   );
 }
